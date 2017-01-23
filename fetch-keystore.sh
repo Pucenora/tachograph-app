f41bdc7c40492be8beded3e89359aecf09fd44db
Fabian Köster <mail@fabian-koester.com>
2017-01-23 22:27:37 +0100

    Always refresh keystores

--- fetch-keystore.sh
+++ fetch-keystore.sh
@@ -3,10 +3,5 @@
 KEYSTORE_PATH="android/app/release.keystore"
 PLAYKEY_PATH="android/app/google-play-key.p12"
 
-if [ ! -f "${KEYSTORE_PATH}" ]; then
-  curl ${KEYSTORE_URL} --user ${KEYSTORE_AUTH} -o "${KEYSTORE_PATH}"
-fi
-
-if [ ! -f "${PLAYKEY_PATH}" ]; then
-  curl ${PLAYKEY_URL} --user ${PLAYKEY_AUTH} -o "${PLAYKEY_PATH}"
-fi
+curl ${KEYSTORE_URL} --user ${KEYSTORE_AUTH} -o "${KEYSTORE_PATH}"
+curl ${PLAYKEY_URL} --user ${PLAYKEY_AUTH} -o "${PLAYKEY_PATH}"
