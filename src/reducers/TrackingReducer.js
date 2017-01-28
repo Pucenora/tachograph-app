2be0273da844eaa8ed22a8438568106bf500e357
Fabian Köster <mail@fabian-koester.com>
2017-01-28 21:08:53 +0100

    Remember tracking status

--- src/reducers/TrackingReducer.js
+++ src/reducers/TrackingReducer.js
@@ -1,6 +1,7 @@
 const INITIAL_STATE = {
   currentAccuracy: -1,
   tripDistanceMeters: 0,
+  isRecording: false,
 };
 
 function updateLastItem(items, updatedItem) {
@@ -19,6 +20,16 @@ export default function TripsReducer(state = INITIAL_STATE, action = {}) {
   switch (action.type) {
     case 'RESET':
       return INITIAL_STATE;
+    case 'TRIP_RECORDING_STARTED':
+      return {
+        ...state,
+        isRecording: true,
+      };
+    case 'TRACKING_STOPPED':
+      return {
+        ...state,
+        isRecording: false,
+      };
     case 'RECEIVE_TRIP_DISTANCE_CHANGED':
       return {
         ...state,
