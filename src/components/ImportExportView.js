9888ed2f0d3052647e83fb9665f2d8163b56f4ad
Fabian Köster <mail@fabian-koester.com>
2017-01-29 22:58:22 +0100

    Add basic CSV export

--- src/components/ImportExportView.js
+++ src/components/ImportExportView.js
@@ -3,9 +3,13 @@ import {
   ScrollView,
   Text,
   StyleSheet,
+  Alert,
 } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import Button from 'react-native-button';
+import RNFS from 'react-native-fs';
+import moment from 'moment';
 import commonStyles from './commonStyles';
 
 const styles = StyleSheet.create({
@@ -16,24 +20,63 @@ class ImportExportView extends React.Component {
 
   static propTypes = {
     // from mapStateToProps:
+    trips: React.PropTypes.array.isRequired,
     // from mapDispatchToProps:
   };
+
+  constructor(props) {
+    super(props);
+    this.export = this.export.bind(this);
+  }
+
+  export() {
+    const path = `${RNFS.ExternalStorageDirectoryPath}/fahrtenbuch.csv`;
+
+    let csvExport = 'Datum,Fahrzeit,Fahrstrecke,Reisezweck,Kilometerstand Fahrt-Beginn,Kilometerstand Fahrt-Ende,km gesch.,km privat,km Arbeitsweg\n';
+
+    this.props.trips.forEach((trip) => {
+      if (trip.type === 'initialOffset') {
+        return;
+      }
+      const startDate = moment(trip.startTimestamp).format('DD.MM.YY');
+      const startTime = moment(trip.startTimestamp).format('hh:mm');
+      const endTime = moment(trip.endTimestamp).format('hh:mm');
+      const tripRoute = '';
+      const tripPurpose = '';
+      const startKm = trip.startOdometerValue;
+      const endKm = trip.endOdometerValue;
+      const distanceBusiness = 0;
+      const distancePrivate = trip.endOdometerValue - trip.startOdometerValue;
+      const distanceCommute = 0;
+      csvExport += `${startDate},${startTime}-${endTime},${tripRoute},${tripPurpose},${startKm},${endKm},${distanceBusiness},${distancePrivate},${distanceCommute}\n`;
+    });
+
+    // write the file
+    RNFS.writeFile(path, csvExport, 'utf8')
+      .then(() => {
+        Alert.alert('Fahrtenbuch wurde erfolgreich nach folgendem Pfad exportiert:', path);
+      })
+      .catch((err) => {
+        Alert.alert(err.message);
+      });
+  }
 
   render() {
     return (
       <ScrollView style={commonStyles.container} keyboardShouldPersistTaps="always">
-        <Text>TODO</Text>
+        <Button
+          style={{ marginTop: 20 }}
+          onPress={this.export}
+        >
+          Exportieren
+        </Button>
       </ScrollView>
     );
   }
 }
 
 const mapStateToProps = state => ({
-
+  trips: state.trips.trips.default,
 });
 
-const mapDispatchToProps = dispatch => ({
-
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ImportExportView);
+export default connect(mapStateToProps, null)(ImportExportView);
